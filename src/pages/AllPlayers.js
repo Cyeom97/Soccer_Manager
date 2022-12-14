b1c9b98aabe6c5c2ec38c836584bd912cc401b10
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-14 15:23:24 -0500

    changed grid for homepage and players page

--- src/pages/AllPlayers.js
+++ src/pages/AllPlayers.js
@@ -15,7 +15,7 @@ const AllPlayers = () => {
 
   return (
     <div className="homepage">
-      <section className="teams">
+      <section className="players">
         {players.map((player) => (
           <div key={player.id}>
             <h2>{player.name}</h2>
