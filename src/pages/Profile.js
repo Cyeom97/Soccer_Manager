c0c44422b6540a6a1400a17d5915324b055c9c21
Christopher Yeom <chrisyeom97@gmail.com>
2022-12-14 14:55:32 -0500

    modified profile css

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -45,7 +45,7 @@ const Profile = ({ user, authenticated }) => {
   addPoints()
 
   return user && authenticated ? (
-    <div className="body">
+    <div className="homePage">
       <h1>My Team</h1>
       <h2>My Points: {myPoints}</h2>
       <div className="pitch">
